6ddea1cd2e9a8fc84fe2b220dc65f5750d566bcf
Ayeza Bashir <ayezabashir442@gmail.com>
2023-06-03 20:31:59 +0500

    fixed syntax error

--- Clock/main.js
+++ Clock/main.js
@@ -5,7 +5,7 @@ const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
 
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 toggle.addEventListener('click', (e) => {
