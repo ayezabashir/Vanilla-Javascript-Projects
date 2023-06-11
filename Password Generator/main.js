c8e35efe7992029166644b713f416566d3e03148
Ayeza Bashir <ayezabashir442@gmail.com>
2023-06-12 00:18:50 +0500

    Update main.js

--- Password Generator/main.js
+++ Password Generator/main.js
@@ -14,6 +14,19 @@ const randomFun = {
     symbol: getRandomSymbol
 }
 
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if (!password) return;
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove()
+})
+
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value;  
     const hasLower = lowercaseEl.checked;
