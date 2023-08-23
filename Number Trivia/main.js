afaa3bfcf3cccf0f6cf7309ffdb16a751aa50797
Ayeza Bashir <ayezabashir442@gmail.com>
2023-08-23 20:10:44 +0000

    fetching the data from api

--- Number Trivia/main.js
+++ Number Trivia/main.js
@@ -1,9 +1,19 @@
 const random_num_fact_url = 'http://numbersapi.com/random/trivia?json';
 const num_fact_url = 'http://numbersapi.com/';
 
+const btn = document.querySelector('.btn');
 const random_btn = document.querySelector('.random-btn');
 const fact = document.querySelector('.fact');
 const inputNum = document.getElementById('number');
+
+btn.addEventListener('click', numFact);
+function numFact() {
+    numUrl = num_fact_url + inputNum.value + '?json';
+    fetch(numUrl)
+        .then(resp => resp.json())
+        .then(data => fact.innerHTML = `${data.text}`)
+        .catch(() => fact.innerHTML = "Sorry, Could not found any information on that number")
+}
 
 random_btn.addEventListener('click', getRandomFact);
 getRandomFact();
